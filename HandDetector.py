7e3b5c1a71773a23c976cc437f8ced6290942d2e
Tornike Maridashvili <tornike.maridashvili@kiu.edu.ge>
2023-09-30 19:26:34 +0400

    add communication with front-end

--- HandDetector.py
+++ HandDetector.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import mediapipe as mp
 import time
+import serial
 
 capture = cv.VideoCapture(0)
 mpHands = mp.solutions.hands
@@ -10,6 +11,10 @@ mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
+
+serial_port = '/dev/cu.usbmodem2101'
+ser = serial.Serial(serial_port, baudrate=9600, timeout=1)
+
 while True:
     success, img = capture.read()
     imgRgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
@@ -18,19 +23,20 @@ while True:
     if result.multi_hand_landmarks:
         for handLms in result.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-            # cv.putText(img, str(handLms), (10, 100), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-            for point_id, lm in enumerate(handLms.landmark):
-                if point_id == 8:
-                    height, witdth, _ = img.shape
-                    x, y = int(lm.x * witdth), int(lm.y * height)
-                    cv.circle(img, (x, y), 10, (0, 255, 0), -1)
-                    cv.putText(img, str((x,y)), (10, 100), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+            index_finger_tip = handLms.landmark[8]
+            index_finger_y = index_finger_tip.y
+            if index_finger_y < handLms.landmark[7].y:
+                cv.putText(img, "Index Finger Up", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                ser.write("U".encode())
+            else:
+                cv.putText(img, "Index Finger Down", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                ser.write("D".encode())
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-    cv.imshow('Webcam', cv.flip(img, 2))
+    cv.imshow('Webcam', img)
     cv.waitKey(1)
 
 
